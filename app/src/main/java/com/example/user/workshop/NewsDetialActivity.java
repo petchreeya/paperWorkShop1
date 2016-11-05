8313af5f90d8a8da6d3ecf09c58ea8f6fa1a9a5c
petchreeya <petchreeya.tip.it57@cpc.ac.th>
2016-11-05 11:36:16 +0700

    first git

---
+++ app/src/main/java/com/example/user/workshop/NewsDetialActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.workshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsDetialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_detial);
+    }
+}
